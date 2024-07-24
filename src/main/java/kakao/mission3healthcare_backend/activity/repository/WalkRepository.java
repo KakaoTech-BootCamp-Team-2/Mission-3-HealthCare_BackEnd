ded6d61ffc8aec2b5ee49b937501e086972bcc44
박지혁 <pjh5365@naver.com>
2024-07-24 17:23:59 +0900

    Feat: 활동 날짜 추가에 따른 쿼리 추가
    Related to: #17

--- src/main/java/kakao/mission3healthcare_backend/activity/repository/WalkRepository.java
+++ src/main/java/kakao/mission3healthcare_backend/activity/repository/WalkRepository.java
@@ -1,9 +1,11 @@
 package kakao.mission3healthcare_backend.activity.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kakao.mission3healthcare_backend.activity.domain.entity.Walk;
 
@@ -17,4 +19,8 @@ public interface WalkRepository extends JpaRepository<Walk, Long> {
 
 	@Query("select w from Walk w where w.member.username = :username")
 	List<Walk> findByUsername(String username);
+
+	@Query("select w from Walk w "
+			+ "where w.member.username = :username and w.walkDate = :date")
+	List<Walk> findByUsernameAndDate(@Param("username") String username, @Param("date") LocalDate date);
 }
