b6e8bf2d575cf2c7f2f4d8022fadef4dc190252b
박지혁 <pjh5365@naver.com>
2024-07-25 13:57:52 +0900

    Refactor: 걷기 엔티티에 제약조건 추가 및 컬럼명 지정
    Related to: #17

--- src/main/java/kakao/mission3healthcare_backend/activity/domain/entity/Walk.java
+++ src/main/java/kakao/mission3healthcare_backend/activity/domain/entity/Walk.java
@@ -2,6 +2,7 @@ package kakao.mission3healthcare_backend.activity.domain.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import kakao.mission3healthcare_backend.auth.domain.entity.Member;
@@ -20,8 +21,11 @@ import lombok.NoArgsConstructor;
 @DiscriminatorValue("walk")
 public class Walk extends Activity {
 
+	@Column(nullable = false)
 	private Double distance; // 거리
+	@Column(name = "avg_heart_rate", nullable = false)
 	private Double avgHeartRate; // 평균 심박수
+	@Column(nullable = false)
 	private LocalDate walkDate; // 활동 날짜
 
 	public void updateWalk(Double distance, Double avgHeartRate, LocalDate walkDate) {
